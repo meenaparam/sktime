102bd8e358f6e38e445cf513e66c10f6b880b17a
meenakshiparameshwaran <meena@prolific.co>
2021-11-03 14:04:57 +0000

    go back to pass for the problematic function; add a new test function

--- sktime/regression/compose/_ensemble.py
+++ sktime/regression/compose/_ensemble.py
@@ -365,8 +365,11 @@ class ComposableTimeSeriesForestRegressor(BaseTimeSeriesForest, BaseRegressor):
 
         self.oob_score_ /= self.n_outputs_
 
-    def _set_oob_score_and_attributes(self, X, y):
-        print("Just something to print")
+    def _set_oob_score_and_attributes(self):
+        pass
+
+    def _test_function(self):
+        print("test function is here")
 
     def _validate_y_class_weight(self, y):
         # in regression, we don't validate class weights
